e943fb59f9f80790afee65721a0eff2138a61797
klkchaitanya <klkchaitanyanmt@gmail.com>
2021-03-08 23:25:33 -0500

    Initial Commit

---
+++ FavoriteDogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FavoriteDogs
+//
+//  Created by Leela Krishna Chaitanya Koravi on 3/8/21.
+//  Copyright © 2021 Leela Krishna Chaitanya Koravi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
